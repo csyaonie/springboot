0f6755c26891692f8a5bf61bcf8c088108449017
郑勇锋 <csyaonie@qq.com>
2020-02-28 17:13:48 +0800

    添加shiro

--- src/main/java/com/csyaonie/bean/User.java
+++ src/main/java/com/csyaonie/bean/User.java
@@ -11,11 +11,21 @@ import java.util.Map;
 @Component
 @ConfigurationProperties(prefix = "user")
 public class User {
+    private Integer id;
     private String name;
+    private String password;
     private Integer age;
     private Map<String,Object> map;
     private List<String> list;
     private List<String> list2;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     public List<String> getList2() {
         return list2;
@@ -55,5 +65,13 @@ public class User {
 
     public void setAge(Integer age) {
         this.age = age;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
